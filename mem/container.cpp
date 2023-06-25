0152ece4f9470bc56477772948eb3d540c11c2c7
memset0 <34177126+memset0@users.noreply.github.com>
2023-06-25 10:48:21 +0800

    basic containers

---
+++ mem/container.cpp
@@ -0,0 +1,18 @@
+#include <map>
+#include <set>
+#include <tuple>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+
+#ifndef MEM_CONTAINER_H
+#define MEM_CONTAINER_H
+
+using std::map;
+using std::pair;
+using std::set;
+using std::tuple;
+using std::unordered_map;
+using std::unordered_set;
+
+#endif

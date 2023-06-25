db28536d0a56d387d1d0017b4d729d24a4ceda8f
memset0 <34177126+memset0@users.noreply.github.com>
2023-06-25 10:48:00 +0800

    global

---
+++ mem/global.hpp
@@ -0,0 +1,23 @@
+#include<iostream>
+
+// short stl
+#define xx first
+#define yy second
+#define pb push_back
+#define eb emplace_back
+#define pii std::pair<int, int>
+#define mp(a, b) std::make_pair(a, b)
+
+// type
+typedef long long ll;
+typedef unsigned long long ull;
+#ifdef __SIZEOF_INT128__
+typedef __int128_t lll;
+typedef __uint128_t ulll;
+#endif
+
+// utils
+using std::cin;
+using std::cout;
+using std::cerr;
+using std::endl;

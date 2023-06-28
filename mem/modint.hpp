cd7c9d4b210c0eced31a6540814ed4f49ecbdcb7
memset0 <34177126+memset0@users.noreply.github.com>
2023-06-28 09:17:01 +0800

    fix: modint.h

--- mem/modint.hpp
+++ mem/modint.hpp
@@ -7,24 +7,21 @@
 template <const int mod> struct Modulus {
   int x;
   Modulus() {}
-  explicit Modulus(int t) { x = t; }
-  explicit Modulus(long long t) { x = safe_mod(t, mod); }
+  Modulus(int t) { x = t; }
+  Modulus(long long t) { x = safe_mod(t, mod); }
 
   constexpr void operator*=(Modulus a) { x = (long long)x * a.x % mod; }
   constexpr void operator-=(Modulus a) { (x -= a.x) < 0 && (x += mod); }
   constexpr void operator+=(Modulus a) { (x += a.x) >= mod && (x -= mod); }
 
-  friend constexpr Modulus operator*(const Modulus &a, const Modulus &b) { return (long long)a.x * b.x % mod; }
+  friend constexpr Modulus operator*(const Modulus &a, const Modulus &b) { return Modulus<mod>((int)((long long)a.x * b.x % mod)); }
   friend constexpr Modulus operator-(Modulus a, const Modulus &b) { return ((a.x -= b.x) < 0 && (a.x += mod)), a; }
   friend constexpr Modulus operator+(Modulus a, const Modulus &b) { return ((a.x += b.x) >= mod && (a.x -= mod)), a; }
 };
 
-template <typename T, const int mod> inline FastRead<T> &operator,(const FastRead<T> &r, Modulus<mod> &x) { return r, x.x; }
-template <typename T, const int mod> inline FastWrite<T> &operator,(const FastWrite<T> &w, const Modulus<mod> &x) { return w, x.x; }
-
 template <const int mod> inline Modulus<mod> finv(Modulus<mod> x) {
   if (x.x < 2) return x;
-  return (mod - mod / x.x) * finv(mod % x.x);
+  return Modulus<mod>(mod - mod / x.x) * finv(Modulus<mod>(mod % x.x));
 }
 
 template <const int mod, typename T> inline Modulus<mod> fpow(Modulus<mod> a, T b) {
@@ -43,6 +40,6 @@ template <const int mod> void init_factorial(int n, Modulus<mod> *fac, Modulus<m
   for (int i = 1; i < n; i++) fac[i] = fac[i - 1] * i, ifac[i] = ifac[i - 1] * ifac[i];
 }
 
-#define Modulus ModInt
+#define ModInt Modulus
 
 #endif
